773c0ff11eb8facfb655bd995979d6c9c69198c3
edward yang <53891309+edwarddded@users.noreply.github.com>
2021-03-06 16:27:40 +1100

    Enemies animation

--- GameStudio1Lab2/Assets/StarCollision.cs
+++ GameStudio1Lab2/Assets/StarCollision.cs
@@ -5,14 +5,11 @@ using UnityEngine;
 public class StarCollision : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.gameObject.tag == "Mario")
+        {
+            Destroy(other);
+        }
     }
 }
