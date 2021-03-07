8e672cc378c2acbc1e0ae14610f67bba4613d64d
edward yang <53891309+edwarddded@users.noreply.github.com>
2021-03-07 20:35:01 +1100

    Camera follow Change

---
+++ GameStudio1Lab2/Assets/CameraFollow.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject followObject;
+    public Vector2 followOffset;
+    public float speed = 3f;
+    private Vector2 threshold;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        threshold = caculateThreshold();
+        rb = followObject.GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        Vector2 follow = followObject.transform.position;
+        float xDifference = Vector2.Distance(Vector2.right * transform.position.x, Vector2.right * follow.x);
+        float yDifference = Vector2.Distance(Vector2.up * transform.position.y, Vector2.right * follow.y);
+
+        Vector3 newPosition = transform.position;
+        if (Mathf.Abs(xDifference) >= threshold.x)
+        {
+            newPosition.x = follow.x;
+        }
+        if (Mathf.Abs(yDifference) >= threshold.y)
+        {
+            newPosition.y = follow.y;
+        }
+        float moveSpeed = rb.velocity.magnitude > speed ? rb.velocity.magnitude : speed;
+        transform.position = Vector3.MoveTowards(transform.position, newPosition, moveSpeed * Time.deltaTime);
+
+    }
+    private Vector3 caculateThreshold()
+    {
+        Rect aspect = Camera.main.pixelRect;
+        Vector2 t = new Vector2(Camera.main.orthographicSize * aspect.width / aspect.height, Camera.main.orthographicSize);
+        t.x -= followOffset.x;
+        t.y -= followOffset.y;
+        return t;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Vector2 border = caculateThreshold();
+        Gizmos.DrawWireCube(transform.position, new Vector3(border.x * 2, border.y * 2, 1));
+    }
+}
