585652df1f3df6c9f69961ac4462ea43f1325486
edward yang <53891309+edwarddded@users.noreply.github.com>
2021-03-08 13:41:24 +1100

    Merge and change scene

---
+++ GameStudio1Lab2/Assets/FlagHit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagHit : MonoBehaviour
+{
+   public Animator animator;
+    // Start is called before the first frame update
+   public  AudioClip flag;
+    public AudioSource flagsource;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Mario")
+        {
+            animator.SetBool("IsHitting", true);
+            flagsource.clip = flag;
+            flagsource.Play();
+        }
+    }
+}
---
+++ GameStudio1Lab2/Assets/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float timer = 400f;
+    public Text time;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        time.text = "Time:" + timer.ToString("0");
+        if (timer < 0f)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
