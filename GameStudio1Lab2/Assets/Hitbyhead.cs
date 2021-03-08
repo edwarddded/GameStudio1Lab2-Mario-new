32ded7239f3bed27e64950143d4e073456e217f7
edward yang <53891309+edwarddded@users.noreply.github.com>
2021-03-08 12:54:25 +1100

    add the enemy and audio effect
    add the enemy and audio effect

---
+++ GameStudio1Lab2/Assets/Hitbyhead.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbyhead : MonoBehaviour
+{
+    float delay = 0.5f;
+    public Animator animator;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Mario")
+        {
+            animator.SetBool("isHitting",true);
+
+            Destroy(gameObject.transform.parent.gameObject, gameObject.transform.parent.gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length + delay);
+        }
+    }
+}
--- GameStudio1Lab2/Assets/Scripts/MarioCollision.cs
+++ GameStudio1Lab2/Assets/Scripts/MarioCollision.cs
@@ -16,6 +16,11 @@ public class MarioCollision : MonoBehaviour
     public Text CoinText;
     float value = 200;
     float Coinscore = 0;
+
+     public AudioSource CoinPlay;
+     public AudioSource MushroomPlay;
+   
+
     private void Start()
     {
         scaleChange = new Vector3(1.5f, 2f, 2f);
@@ -29,18 +34,24 @@ public class MarioCollision : MonoBehaviour
             Coinscore += value;
 
             CoinText.text = "Coin Score:" + Coinscore.ToString();
+
+            CoinPlay.Play();
             Debug.Log(Coinscore);
             Debug.Log(other.name);
         }
         if (other.gameObject.tag == "Star")
         {
             Destroy(other.gameObject);
-            sr.color = new Color(1, 0, 0, 1);
+            //sr.color = new Color(1, 0, 0, 1);
+            health++;
+            healthText.text = "Health X" + health.ToString();
+            MushroomPlay.Play();
             Debug.Log(other.name);
         }
         if (other.gameObject.tag == "Mushroom")
         {
             Destroy(other.gameObject);
+            MushroomPlay.Play();
             gameObject.transform.localScale = scaleChange;
             Debug.Log(other.name);
         }
@@ -55,6 +66,8 @@ public class MarioCollision : MonoBehaviour
 
             Debug.Log(other.name);
         }
+            
+        
         if (other.gameObject.tag == "Black")
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
